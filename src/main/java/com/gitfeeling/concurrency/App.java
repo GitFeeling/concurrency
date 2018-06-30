1327c8b47dc31437a4d0b5830e6a0bc7161eb8a4
Sachit Khanna <eugene@Sachits-MacBook-Pro.local>
2018-06-30 16:55:40 -0500

    First commit

---
+++ src/main/java/com/gitfeeling/concurrency/App.java
@@ -0,0 +1,13 @@
+package com.gitfeeling.concurrency;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
